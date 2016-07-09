40af02b8c3fa8ea2d11218ea9e77176d68f28ce4
Ivo Georgiev <ivo@linvo.org>
2016-07-10 01:36:29 +0300

    you can now navigate by arrows, updated style

--- public/js/board.js
+++ public/js/board.js
@@ -47,11 +47,11 @@ function pullMetaGroup(addon, args, sort) {
 		if (! res.length) return;
 
 		var group = groups[id] = {
+			id: id,
 			type: args.query.type,
 			sort: sort,
 			addon: addon,
 			items: res.map(function(x) { return new metadata(x) }).filter(function(x) { return x._poster }).slice(0,6),
-
 		};
 
 		updateView();
@@ -64,7 +64,7 @@ function metadata(d) {
 	for (key in d) this[key] = d[key];
 
 	self.popularities = self.popularities || {};
-	self._id = self.id || self._id;
+	self._id = self.id || self._id || self.imdb_id || (self.yt_id && 'yt_id:'+self.yt_id) || (self.filmon_id && 'filmon_id:'+self.filmon_id) || 'rand-'+Date.now();
 
 	self.getPoster = function() {
 		if (self.imdb_id) poster = "http://img.omdbapi.com/?i="+self.imdb_id+"&apikey=a38ae05f&h=400";
@@ -91,13 +91,56 @@ function updateView() {
 }
 
 var selected;
-function updateSelected() {
+function firstItem() {
 	var groupKeys = Object.keys(groups);
-
+	var firstGroup = groups[groupKeys[0]];
+	var firstItem = firstGroup && firstGroup.items && firstGroup.items[0];
+	return firstItem;
+}
+function updateSelected() {
 	if (!selected)  {
-		var firstGroup = groups[groupKeys[0]];
-		var firstItem = firstGroup && firstGroup.items && firstGroup.items[0];
-		document.querySelector('.boardItem[data-id='+firstGroup.items[0]._id+']').classList.add('focused');
+		var first = firstItem();
+		if (first) document.querySelector('.boardItem[data-id="'+first._id+'"]').classList.add('focused');
+		return;
 	}
 
+	document.querySelector('.boardItem.focused').classList.remove('focused');
+	document.querySelector('.boardItem[data-id="'+selected+'"]').classList.add('focused');
 }
+
+
+document.onkeydown = function(e) {
+	var first = firstItem();
+	if (! selected) selected = first._id;
+
+	// find current x, y; don't keep them as state, because we can load new rows anytime
+	var x, y;
+	var groupKeys = Object.keys(groups);
+	groupKeys.forEach(function(k, i) {
+		(groups[k].items || []).forEach(function(item, j) {
+			if (item._id && (item._id === selected)) { y = i; x = j; }
+		})
+	});
+
+	// apply command from user
+	if (e.keyCode === 37) x--; // left
+	else if (e.keyCode === 39) x++; // right
+	else if (e.keyCode === 38) y--; // up
+	else if (e.keyCode === 40) y++; // down
+	else return;
+
+	e.preventDefault();
+
+	// set new 'selected', update it in the DOM
+	var group = groups[groupKeys[y]];
+	if (! group) return;
+
+	var item = group.items[x];
+	if (! item) return;
+
+	selected = item._id;
+	updateSelected();
+
+	location.href = "#";
+	location.href = "#"+groupKeys[y];
+};
