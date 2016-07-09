192f18e698dba1d6c3ad62a79a8cd4591a034eb1
Ivo Georgiev <ivo@linvo.org>
2016-07-10 00:41:57 +0300

    fix identation

--- public/js/board.js
+++ public/js/board.js
@@ -12,48 +12,48 @@ var stremio = new StremioClient();
 ].forEach(function(url) { stremio.add(url+'/stremioget/stremio/v1') }) // add legacy add-on suffix
 
 stremio.on("addon-ready", function(addon) {
-    if (addon.methods.indexOf("meta.find") === -1) return;
+	if (addon.methods.indexOf("meta.find") === -1) return;
 
-    var sorts = addon.manifest.sorts;
+	var sorts = addon.manifest.sorts;
 
-    (addon.manifest.sorts || [null]).forEach(function(sort) {
-        ((sort && sort.types) || addon.manifest.types).forEach(function(type) {
-            if (!type) return;
-            
-            var args = { query: { type: type }, limit: 50 };
-            if (sort) {
-            	args.sort = { };
-            	args.sort[sort.prop] = -1;
-            	// args.sort.popularity = -1
-            }
+	(addon.manifest.sorts || [null]).forEach(function(sort) {
+		((sort && sort.types) || addon.manifest.types).forEach(function(type) {
+			if (!type) return;
+			
+			var args = { query: { type: type }, limit: 50 };
+			if (sort) {
+				args.sort = { };
+				args.sort[sort.prop] = -1;
+				// args.sort.popularity = -1
+			}
 
-            pullMetaGroup(addon, args, sort);
-        });
-    });
+			pullMetaGroup(addon, args, sort);
+		});
+	});
 });
 
 function pullMetaGroup(addon, args, sort) {
-    var id = addon.identifier()+":"+args.query.type+":"+(sort && sort.prop);
+	var id = addon.identifier()+":"+args.query.type+":"+(sort && sort.prop);
 
-    if (groups.hasOwnProperty(id)) return;
-    groups[id] = { items: [] };
+	if (groups.hasOwnProperty(id)) return;
+	groups[id] = { items: [] };
 
-    stremio.fallthrough([addon], "meta.find", args, function(err, res) {
-        if (err) return console.error(err);
+	stremio.fallthrough([addon], "meta.find", args, function(err, res) {
+		if (err) return console.error(err);
 
-        console.log("pulled "+res.length+" items for "+addon.identifier()+" for "+args.query.type);
-        if (! res.length) return;
+		console.log("pulled "+res.length+" items for "+addon.identifier()+" for "+args.query.type);
+		if (! res.length) return;
 
-       	var group = groups[id] = {
-       		type: args.query.type,
-       		sort: sort,
-       		addon: addon,
-       		items: res.slice(0,6).map(function(x) { return new metadata(x) }),
+		var group = groups[id] = {
+			type: args.query.type,
+			sort: sort,
+			addon: addon,
+			items: res.map(function(x) { return new metadata(x) }).filter(function(x) { return x._poster }).slice(0,6),
 
-       	};
+		};
 
-       	updateView();
-    });
+		updateView();
+	});
 }
 
 function metadata(d) {
@@ -82,7 +82,7 @@ var timeout;
 function updateView() {
 	//if (view) view.update({ groups: groups })
 	if (timeout) clearTimeout(timeout);
-    timeout = setTimeout(function() { if (view) view.update({ groups: groups }) }, 100);
+	timeout = setTimeout(function() { if (view) view.update({ groups: groups }) }, 100);
 }
 
 
